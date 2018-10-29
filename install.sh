1b3d70b17522b3b8cc9b2174b42cfd2f5bb95a01
zhaoweiqiang <zwq00000@vm.dell>
2018-10-29 17:58:31 +0800

    add installer

--- install.sh
+++ install.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+
+# $1:linktarget
+# $2 source sh
+function checkLink(){
+    source=$1
+    target=$2
+    if [ -f $target ]; then
+        echo "File $target exists."
+    else
+        echo "link $target to $source"
+        sudo ln $source $target -s
+    fi
+}
+
+checkLink "go.sh" "/usr/bin/go"
+
+checkLink "yarn.sh" "/usr/bin/yarn"
+
+checkLink "node.sh" "/usr/bin/node"
+checkLink "npm.sh" "/usr/bin/npm"
+checkLink "npx.sh" "/usr/bin/npx"
+
+checkLink "python.sh" "/usr/bin/python"
+
+checkLink "git.sh" "/usr/bin/git"
+
+checkLink "dotnet.sh" "/usr/bin/dotnet"
+
+
