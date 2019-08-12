02556302e6ceab31d95a6a6dd5dcdbef0effe53e
zwq00000 <zwq00000@DevVM102.localdomain>
2019-08-12 13:35:59 +0800

    add php 7.2

---
+++ php/php-fpm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Use docker image php:7.2-fpm"
+
+localpath=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/node/.npm/local/bin
+
+docker run -it --rm -u $UID --userns=host \
+-e PATH=$localpath \
+--network=host \
+-v /etc/localtime:/etc/localtime:ro \
+-v `pwd`:`pwd` -w `pwd` php:7.2-fpm $*
