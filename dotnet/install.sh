b61640839d634d8a4c04f149c6ec36ef3677275f
zhao weiqiang <zwq00000@hotmail.com>
2020-02-25 18:06:09 +0800

    update dotnet sdk to 3.1

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -5,8 +5,8 @@ cd $source_path
 
 BIN_PATH=$1
 
-echo ln -s -f `pwd`/dotnetenv.sh $BIN_PATH/dotnetenv 
-ln -s -f `pwd`/dotnetenv.sh $BIN_PATH/dotnetenv 
+echo ln -s -f `pwd`/dotnetshell.sh $BIN_PATH/dotnetshell 
+ln -s -f `pwd`/dotnetshell.sh $BIN_PATH/dotnetshell 
 
 echo ln -s -f `pwd`/dotnet.sh $BIN_PATH/dotnet 
 ln -s -f `pwd`/dotnet.sh $BIN_PATH/dotnet 
