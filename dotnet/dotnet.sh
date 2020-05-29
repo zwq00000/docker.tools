ed7731f3a577cf663385cf1297f2f38780e73500
zhao weiqiang <zwq00000@VM_22_RDMP.Local>
2020-05-29 10:55:46 +0800

    fix format

--- dotnet/dotnet.sh
+++ dotnet/dotnet.sh
@@ -3,14 +3,14 @@ imageName=mcr.microsoft.com/dotnet/core/sdk:3.1
 
 SHARE_VALUME=dotnet_home_for_$USER
 
-docker run  --rm -t --name dotnetenv_$cur_dateTime \
+docker run  --rm -t --name dotnet_$cur_dateTime \
 -u $UID:$UID --userns=host \
 --network host \
 -e HOME=$HOME \
 -v /etc/localtime:/etc/localtime:ro \
 -v /etc/passwd:/etc/passwd:ro \
 -v /etc/group:/etc/group:ro \
--v  /etc/localtime:/etc/localtime:ro \ 
+-v  /etc/localtime:/etc/localtime:ro \
 -v $SHARE_VALUME:$HOME \
 -v `pwd`:`pwd` -w `pwd` \
 $imageName dotnet $*
