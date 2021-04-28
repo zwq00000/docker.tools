2831edec941243d87a85034e34e648c97f2ef327
zhao weiqiang <zwq00000@gmail.com>
2021-04-28 18:14:49 +0800

    fix for node install

--- node/install.sh
+++ node/install.sh
@@ -11,8 +11,19 @@ echo "alias yarn='$PWD/yarn.sh'" >> ~/.bash_alias
 echo "alias npx='$PWD/npx.sh'" >> ~/.bash_alias
 echo "alias npm-runtime='$PWD/npm-runtime.sh'" >> ~/.bash_alias
 
-. ~/.bash_alias
+source ~/.bash_alias
 
-nodeenv init
-npm config set prefix $HOME/.npm/global
-npm config set registry https://registry.npm.taobao.org
+imageName=library/node:12
+SHARE_VALUME=npm_home_for_$USER
+ # Create User Profile Volume
+docker volume create $SHARE_VALUME
+
+docker run  --rm -it --userns=host \
+    -e HOME=$HOME \
+    -v $SHARE_VALUME:$HOME  \
+    -w $HOME \
+    $imageName echo "chown  $UID:$UID $HOME/" && \
+        chown  $UID:$UID $HOME/ 
+
+# npm config set prefix $HOME/.npm/global 
+# npm config set registry https://registry.npm.taobao.org"
