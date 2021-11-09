9d0317c568592dd7c285e76d79a89b42215f0984
赵卫强 <zwq00000@hotmail.com>
2021-11-10 00:11:00 +0800

    fix yarn and add pnpm

---
+++ go/go.1.15.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+imageName=golang:1.15
+
+SHARE_VALUME=go_cache_for_$USER
+ROOT_VALUME=go_root_for_$USER
+gopath=$HOME/.go
+localpath=/go/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$gopath/bin
+
+docker run -it --rm \
+        -u $UID:$UID --userns=host \
+        --network=host \
+        -e GOENV=$gopath/env \
+        -e GOCACHE=$gopath/go-build \
+        -e GOPATH=$gopath \
+        -e GOBIN=$gopath/bin \
+        -e GOMODCACHE=$gopath/pkg/mod \
+        -e GO111MODULE=on \
+        -e GOPROXY=https://goproxy.cn,direct \
+        -e PATH=$localpath \
+        -v /etc/localtime:/etc/localtime:ro \
+        -v /etc/passwd:/etc/passwd:ro \
+        -v /etc/group:/etc/group:ro \
+        -v $SHARE_VALUME:$gopath \
+        -v `pwd`:`pwd` -w `pwd` \
+        $imageName  $*
--- node/pnpm.sh
+++ node/pnpm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ## user ~/.npmrc and ~/.npm folder
 imageName=library/node:12
-echo "Entry docker container ${imageName}"
+# echo "Entry docker container ${imageName}"
 localpath=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$HOME/.npm/global/bin
 
 SHARE_VALUME=npm_home_for_$USER
-docker volume create $SHARE_VALUME
+# docker volume create $SHARE_VALUME
 
 docker run -it --rm -u $UID:$UID --userns=host \
 --network=host \
@@ -15,4 +15,4 @@ docker run -it --rm -u $UID:$UID --userns=host \
 -v /etc/group:/etc/group:ro \
 -v $SHARE_VALUME:$HOME \
 -v `pwd`:`pwd` -w `pwd` \
-$imageName yarn $*
+$imageName pnpm $*
